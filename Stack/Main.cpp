86d56b3b8547afe4d5f4295eeac6c4fcddd92ce4
Brandon Evans <bevans@customorthopaedics.com>
2015-03-11 11:14:56 -0400

    chapter 4 - maintain linked list tail pointer

--- Stack/Main.cpp
+++ Stack/Main.cpp
@@ -3,21 +3,109 @@
 
 using namespace std;
 
-int main() {
-    Stack stack;
-    stack.push(3);
-    stack.push(9);
-    stack.push(87);
-    stack.push(1);
-    stack.push(6);
-    stack.push(78);
-    stack.push(3);
-    stack.push(0);
-    int num = 0;
-    while( stack.topNode() ) {
-        cout << stack.pop() << endl;
-        ++num;
+Node* head = nullptr;
+Node* tail = nullptr;
+
+bool deleteNode( Node *node ) {
+    if(node){
+    } else {
+        return false;
     }
+    Node *prev = nullptr;
+    Node* curr = head;
+    if ( curr == node) {
+        if (tail == head)
+            tail = head->getNext();
+        head = head->getNext();
+        delete curr;
+        return true;
+    }
+
+    while( curr ) {
+        if( curr->getNext() == node ) {
+            if(node == tail){
+                tail = curr;
+            }
+            curr->setNext(node->getNext());
+            delete node;
+            return true;
+        } else {
+            curr = curr->getNext();
+        }
+    }
+    return false;
+}
+
+bool insertAfter( Node *node, int data ) {
+    Node *prev = nullptr;
+    Node* curr = new Node(data);
+    if(node){
+    } else {
+        curr->setNext(head);
+        if (!head) { // if list is empty, set tail to first item
+            tail = curr;
+        }
+        head = curr;
+        return true;
+    }
+    prev = head;
+    while( prev ) {
+        if( prev == node ) {
+            curr->setNext(prev->getNext());
+            prev->setNext(curr);
+            if(prev == tail){
+                tail = curr;
+            }
+            return true;
+        } else {
+            prev = prev->getNext();
+        }
+    }
+    delete curr;
+    return false;
+}
+
+
+int main() {
+    //Stack stack;
+    //stack.push(3);
+    //stack.push(9);
+    //stack.push(87);
+    //stack.push(1);
+    //stack.push(6);
+    //stack.push(78);
+    //stack.push(3);
+    //stack.push(0);
+
+    if(deleteNode(head)) {
+    } else {
+        cout << "Can't delete from empty list" << endl;
+    }
+    int num = 0;
+    Node *node = nullptr;
+    for(int i = 0; i < 10; i++) {
+        if ( insertAfter(node,i) ){
+        } else {
+            cout << "\nTHere was an insert error!\n";
+        }
+        node = tail;
+    }
+    cout << "Head and tail: " << head->value() << " " << tail->value() << endl;
+    if(deleteNode(head)) {
+    } else {
+        cout << "error deleting" << endl;
+    }
+    cout << "Head and tail: " << head->value() << " " << tail->value() << endl;
+    if(deleteNode(tail)) {
+    } else {
+        cout << "error deleting tail" << endl;
+    }
+    cout << "Head and tail: " << head->value() << " " << tail->value() << endl;
+    
+    //while( stack.topNode() ) {
+    //    cout << stack.pop() << endl;
+    //    ++num;
+    //}
     int n;
     cin >> n;
     return num;
